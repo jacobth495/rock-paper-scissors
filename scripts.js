6d56a97f9dfa0453823c02faa2fd5a54666b6f2d
Jacob Henderson <jacobth495@gmail.com>
2022-08-09 15:07:35 -0500

    Created playRound function

--- scripts.js
+++ scripts.js
@@ -3,3 +3,21 @@ function getComputerChoices(a,b,c) {
   y = x[Math.floor(Math.random() * 3)]
   return y
 }
+function playRound(playerSelection, computerSelection) {
+  x = playerSelection;
+  y = computerSelection;
+  if(x ===`rock` && y === `paper`) {
+    console.log('You win rock beats paper');
+  }
+  else if(x ===`paper` && y === `rock`) {
+    console.log('You win paper beats rock');
+  }
+  else if(x ===`scissors` && y === `paper`) {
+    console.log('You win scissors beats paper')
+  }
+  else console.log('You Lose')
+}
+ 
+const playerSelection = prompt('Rock Paper or Scissors').toLowerCase();
+const computerSelection = getComputerChoices();
+console.log(playRound(playerSelection, computerSelection));
