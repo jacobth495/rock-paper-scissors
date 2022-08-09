00bf6697bcd2c3a624292f5a010f351fff3beabd
Jacob Henderson <jacobth495@gmail.com>
2022-08-09 18:59:29 -0500

    Updated the playRound function

--- scripts.js
+++ scripts.js
@@ -1,23 +1,46 @@
 function getComputerChoices(a,b,c) {
-  x = [`rock`, `paper`, `scissors`]
-  y = x[Math.floor(Math.random() * 3)]
+  const x = [`rock`, `paper`, `scissors`]
+  const y = x[Math.floor(Math.random() * 3)]
   return y
 }
+
 function playRound(playerSelection, computerSelection) {
   x = playerSelection;
   y = computerSelection;
-  if(x ===`rock` && y === `paper`) {
-    console.log('You win rock beats paper');
+  if (x == y) {
+    console.log("Tie")
+  }
+  else if(x ===`rock` && y === `scissors`) {
+    playerScore++;
+    console.log('You win rock beats scissors');
   }
   else if(x ===`paper` && y === `rock`) {
+    playerScore++;
     console.log('You win paper beats rock');
   }
-  else if(x ===`scissors` && y === `paper`) {
+  else if (x ===`scissors` && y === `paper`) {
+    playerScore++;
     console.log('You win scissors beats paper')
   }
-  else console.log('You Lose')
+  else if (y ===`scissors` && x === `paper`){
+    computerScore++;
+    console.log('You lost scissors beats paper')
+  }
+  else if (y ===`rock` && x === `scissors`){
+    computerScore++;
+    console.log('You lost rock beats scissors')
+  }
+  else if (y ===`paper` && x === `rock`){
+    computerScore++;
+    console.log('You lost paper beats rock')
+  }
 }
- 
-const playerSelection = prompt('Rock Paper or Scissors').toLowerCase();
-const computerSelection = getComputerChoices();
+
+
+let playerScore = 0;
+let computerScore = 0;
+
+let playerSelection = prompt(`Rock Paper or Scissors`).toLowerCase();
+let computerSelection = getComputerChoices();
 console.log(playRound(playerSelection, computerSelection));
+//console.log(game())
